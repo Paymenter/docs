d69c702b1f8bf9deee921ab467c3c3422b21fd02
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2023-01-25 10:06:34 +0100

    Remove padding from main element

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" integrity="sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
             </Head>
-            <main className='flex flex-col justify-between overflow-hidden relative'>
+            <main className='flex flex-col justify-between overflow-hidden relative mainclass'>
                 <div className="container mx-auto md:px-0 px-5">
                     <div className="header text-center mt-4">
                         <h1 className="text-4xl">Next-gen Gateway for hostings that boosts your workflow</h1>
