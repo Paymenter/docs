d9e87c9bb5e505d71d288787bc87831b8e9edd20
Darkle <tomasromero@outlook.com>
2024-10-30 02:03:46 +0100

    fix: hero links

--- .vitepress/theme/components/landing/Hero.vue
+++ .vitepress/theme/components/landing/Hero.vue
@@ -14,12 +14,12 @@ import { onMounted } from 'vue';
       
 
   <div class="buttons">
-                <a href="https://github.com/sponsors/Paymenter" target="_blank" class="btn btn-primary svg-left" style="color:white;">
+                <a href="/docs/installation/install" class="btn btn-primary svg-left" style="color:white;">
                     Get Started
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M4 3C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21H20C20.5523 21 21 20.5523 21 20V4C21 3.44772 20.5523 3 20 3H4ZM16.0001 8V16.4142L12.5001 12.9142L8.70718 16.7071L7.29297 15.2929L11.0859 11.5L7.58586 8H16.0001Z"></path></svg>
                 
                 </a>
-                <a href="https://github.com/Paymenter/Paymenter" target="_blank" class="btn svg-left">
+                <a href="https://demo.paymenter.org/" target="_blank" class="btn svg-left">
                     Demo
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 13H4V11H12V4L20 12L12 20V13Z"></path></svg>
       </a>
