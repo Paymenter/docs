e0bed9995cff6ca954c65c68207efe8dc595032d
iamdarkle <tomasromero@outlook.com>
2025-04-03 04:24:49 +0200

    feat: FeatureItem component

---
+++ .vitepress/theme/components/landing/FeatureItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col gap-5">
+    <div class="flex items-center gap-3 text-sm uppercase tracking-widest text-[var(--vp-c-text-2)]">
+      <component :is="icon" class="size-5" />
+      {{ title }}
+    </div>
+    <span class="font-normal text-sm text-[var(--vp-c-text-3)]">
+      {{ description }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  defineProps({
+    icon: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  });
+</script>
