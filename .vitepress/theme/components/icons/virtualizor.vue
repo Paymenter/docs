dc8f235a30894d3a6a7149edc45eaf999976a461
iamdarkle <tomasromero@outlook.com>
2025-04-30 04:30:17 +0200

    feat: new icons

---
+++ .vitepress/theme/components/icons/virtualizor.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg width="67" height="80" viewBox="0 0 67 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 19L61 80H0V19Z" fill="url(#paint0_linear_903_3800)"/>
+<path d="M67 61L6 0H67V61Z" fill="url(#paint1_linear_903_3800)"/>
+<defs>
+<linearGradient id="paint0_linear_903_3800" x1="31" y1="50" x2="0" y2="80" gradientUnits="userSpaceOnUse">
+<stop stop-color="#53668E"/>
+<stop offset="1" stop-color="#2D384E"/>
+</linearGradient>
+<linearGradient id="paint1_linear_903_3800" x1="67" y1="-5.48363e-06" x2="37" y2="31" gradientUnits="userSpaceOnUse">
+<stop stop-color="#FF9B00"/>
+<stop offset="1" stop-color="#F77C02"/>
+</linearGradient>
+</defs>
+</svg>
+</template>
